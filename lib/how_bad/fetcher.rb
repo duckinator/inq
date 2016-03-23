d727c3a0c8362dc496168e3c4260e03446cd6b97
Ellen Marie Dash <me@duckie.co>
2016-03-23 16:27:42 -0400

    Convert issues and PRs to arrays of hashes.

--- lib/how_bad/fetcher.rb
+++ lib/how_bad/fetcher.rb
@@ -12,8 +12,12 @@ class HowBad::Fetcher
     pulls   = github.pulls.list  user: user, repo: repo
 
     {
-      issues: issues,
-      pulls:  pulls,
+      issues: to_array_of_hashes(issues),
+      pulls:  to_array_of_hashes(pulls),
     }
+  end
+
+  private def to_array_of_hashes(object)
+    object.to_a.map(&:to_h)
   end
 end
