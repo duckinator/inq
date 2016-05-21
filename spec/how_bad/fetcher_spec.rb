32eee4e73c46ed161b48140d2442ad37e827388c
Ellen Marie Dash <me@duckie.co>
2016-05-20 20:27:52 -0400

    Add fetcher spec.

--- spec/how_bad/fetcher_spec.rb
+++ spec/how_bad/fetcher_spec.rb
@@ -2,8 +2,21 @@ require 'spec_helper'
 require 'json'
 
 describe HowBad::Fetcher do
-  github = instance_double('GitHub',
-            issues: instance_double('GitHub::Issues', list: JSON.parse(open('../data/issues.json'))),
-            pulls: instance_double('GitHub::Pulls', list: JSON.parse(open('../data/pulls.json')))
-            )
+  let(:issues) { JSON.parse(open(File.expand_path('../data/issues.json', __dir__)).read) }
+  let(:pulls) { JSON.parse(open(File.expand_path('../data/pulls.json', __dir__)).read) }
+
+  let(:github) {
+    instance_double('GitHub',
+      issues: instance_double('GitHub::Issues', list: issues),
+      pulls: instance_double('GitHub::Pulls', list: pulls)
+    )
+  }
+
+  context '#call' do
+    it 'returns a hash containing issues and pull requests' do
+      results = subject.call('user/repo', github)
+      expect(results[:issues]).to eq(issues)
+      expect(results[:pulls]).to eq(pulls)
+    end
+  end
 end
