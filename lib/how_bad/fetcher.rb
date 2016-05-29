ea59a26387ca0484cf5a2f8471c54223718399a3
Ellen Marie Dash <me@duckie.co>
2016-05-29 03:04:52 -0400

    Rename to_array_of_hashes so it's clearer how it works.

--- lib/how_bad/fetcher.rb
+++ lib/how_bad/fetcher.rb
@@ -12,12 +12,12 @@ class HowBad::Fetcher
     pulls   = github.pulls.list  user: user, repo: repo
 
     {
-      issues: to_array_of_hashes(issues),
-      pulls:  to_array_of_hashes(pulls),
+      issues: obj_to_array_of_hashes(issues),
+      pulls:  obj_to_array_of_hashes(pulls),
     }
   end
 
-  private def to_array_of_hashes(object)
+  private def obj_to_array_of_hashes(object)
     object.to_a.map(&:to_h)
   end
 end
