f91b953c7ff7f28f4194d2d6611f369936e145b9
Ellen Marie Dash <me@duckie.co>
2016-05-29 05:12:17 -0400

    Beginning of specs for Analyzer.

--- spec/how_bad/analyzer_spec.rb
+++ spec/how_bad/analyzer_spec.rb
@@ -6,5 +6,29 @@ describe HowBad::Analyzer do
 
   let(:fetcher_results) { HowBad::Fetcher::Results.new(issues, pulls) }
 
-  
+  subject { HowBad::Analyzer.new }
+
+  context '#num_with_label' do
+    it 'returns a Hash mapping labels to the number of issues or pulls with that label' do
+      result = subject.num_with_label(issues)
+
+      expect(result).to eq(TODO)
+    end
+  end
+
+  context '#average_age_for' do
+   it 'returns the average age for the provided issues or pulls' do
+     result = subject.average_age_for(issues)
+
+     expect(result).to eq(TODO)
+   end
+  end
+
+  context '#oldest_age_for' do
+    it 'returns the oldest age for the provided issues or pulls' do
+      result = subject.average_age_for(issues)
+
+      expect(result).to eq(TODO)
+    end
+  end
 end
