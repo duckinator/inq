c9c4f9100067dcae3ec3b1af160f479dc8759aae
Ellen Marie Dash <me@duckie.co>
2016-05-29 04:19:58 -0400

    Fix alignment of code.

--- lib/how_bad/analyzer.rb
+++ lib/how_bad/analyzer.rb
@@ -10,16 +10,17 @@ module HowBad
 
     Contract Fetcher::Results, Class => Analysis
     def call(data, analysis_class: Analysis)
-       analysis_class.new(
-         total_issues:  data.issues.length,
-         total_pulls:   data.pulls.length,
 
-         average_issue_age: average_age_for(issues),
-         oldest_issue_date: oldest_date_for(issues),
+      analysis_class.new(
+        total_issues:  data.issues.length,
+        total_pulls:   data.pulls.length,
 
-         average_pull_age:  average_age_for(pulls),
-         oldest_pull_date:  oldest_date_for(pulls),
-       )
+        average_issue_age: average_age_for(issues),
+        oldest_issue_date: oldest_date_for(issues),
+
+        average_pull_age:  average_age_for(pulls),
+        oldest_pull_date:  oldest_date_for(pulls),
+      )
     end
 
     def average_age_for(issues_or_pulls)
