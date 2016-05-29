d82fba9c21393633b3087b7beab2bd0d4d9dc0c9
Ellen Marie Dash <me@duckie.co>
2016-05-29 04:48:14 -0400

    Flesh out Analysis slightly.

--- lib/how_bad/analyzer.rb
+++ lib/how_bad/analyzer.rb
@@ -12,10 +12,12 @@ module HowBad
 
     Contract Fetcher::Results, Class => Analysis
     def call(data, analysis_class: Analysis)
-
       analysis_class.new(
         total_issues:  data.issues.length,
         total_pulls:   data.pulls.length,
+
+        issues_with_label: num_with_label(issues),
+        pulls_with_label: num_with_label(pulls),
 
         average_issue_age: average_age_for(issues),
         oldest_issue_date: oldest_date_for(issues),
@@ -23,6 +25,19 @@ module HowBad
         average_pull_age:  average_age_for(pulls),
         oldest_pull_date:  oldest_date_for(pulls),
       )
+    end
+
+  private
+
+    def num_with_label(issues_or_pulls)
+      {} # TODO: Implement.
+      # Notes:
+      # Hash mapping labels to the number of issues with those labels.
+      # E.g., given 10 with label "label1" and 5 with label "label2",
+      # {
+      #   "label1" => 10,
+      #   "label2" => 5
+      # }
     end
 
     def average_age_for(issues_or_pulls)
