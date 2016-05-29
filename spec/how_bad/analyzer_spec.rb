b8fe0b98c2e3da3e6e837c51fa72a26bf29489d9
Ellen Marie Dash <me@duckie.co>
2016-05-29 04:50:15 -0400

    Define variables needed for Analyzer specs.

--- spec/how_bad/analyzer_spec.rb
+++ spec/how_bad/analyzer_spec.rb
@@ -1,4 +1,10 @@
 require 'spec_helper'
 
 describe HowBad::Analyzer do
+  let(:issues) { JSON.parse(open(File.expand_path('../data/issues.json', __dir__)).read) }
+  let(:pulls) { JSON.parse(open(File.expand_path('../data/pulls.json', __dir__)).read) }
+
+  let(:fetcher_results) { HowBad::Fetcher::Results.new(issues, pulls) }
+
+  
 end
