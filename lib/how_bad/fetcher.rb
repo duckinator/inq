d2e6e70851047014bc34e091c802f1ca77bc6537
Ellen Marie Dash <me@duckie.co>
2016-06-03 09:57:17 -0400

    Return repository in Fetcher::Results.

--- lib/how_bad/fetcher.rb
+++ lib/how_bad/fetcher.rb
@@ -11,12 +11,12 @@ class HowBad::Fetcher
   #
   # Implemented as a class instead of passing around a Hash so that it can
   # be more easily referenced by Contracts.
-  class Results < Struct.new(:issues, :pulls)
+  class Results < Struct.new(:repository, :issues, :pulls)
     include Contracts::Core
 
-    Contract C::ArrayOf[Hash], C::ArrayOf[Hash] => nil
-    def initialize(issues, pulls)
-      super(issues, pulls)
+    Contract String, C::ArrayOf[Hash], C::ArrayOf[Hash] => nil
+    def initialize(repository, issues, pulls)
+      super(repository, issues, pulls)
     end
 
     # Struct defines #to_h, but not #to_hash, so we alias them.
@@ -32,6 +32,7 @@ class HowBad::Fetcher
     pulls   = github.pulls.list  user: user, repo: repo
 
     Results.new(
+      repository,
       obj_to_array_of_hashes(issues),
       obj_to_array_of_hashes(pulls)
     )
