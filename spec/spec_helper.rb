fd76b25daf0f36639ce25c514b3eb98c4bacf524
Ellen Marie Dash <me@duckie.co>
2016-08-07 04:47:53 -0400

    Remove unnecessary code.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,3 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'how_is'
 require 'timecop'
 
-RSpec.configure do |config|
-  # For tags. https://engineering.sharethrough.com/blog/2013/08/10/greater-test-control-with-rspecs-tag-filters/
-  config.treat_symbols_as_metadata_keys_with_true_values = true
-end
