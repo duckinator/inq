ccca98e23ffdfbe8373a0e21aa16b1f9d340e586
Ellen Marie Dash <me@duckie.co>
2016-08-07 00:15:14 -0400

    Add another integration test.

--- spec/integration/command_line_spec.rb
+++ spec/integration/command_line_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 require 'open3'
 
 HOW_IS_CONFIG_FILE = File.expand_path('../data/integration/how_is.yml', __dir__)
+HOW_IS_EXAMPLE_REPOSITORY_JSON_REPORT = File.expand_path('../data/example-repository-report.json', __dir__)
 
 JEKYLL_HEADER = <<-EOF
 ---
@@ -30,6 +31,30 @@ describe 'Command line', :integration do
           json_report = File.open('report.json').read
 
           expect(html_report).to include(JEKYLL_HEADER)
+        }
+      }
+    end
+  end
+
+  context 'running `how_is how-is/example-repository --report report.json`' do
+    it 'generates a valid report file' do
+      Dir.mktmpdir {|dir|
+        Dir.chdir(dir) {
+          data = {}
+
+          Open3.popen3("bundle exec how_is how-is/example-repository --report report.json") do |stdin, stdout, stderr, wait_thr|
+            wait_thr.join # Wait for command to finish executing.
+
+            data[:stdout] = stdout.read
+            data[:stderr] = stderr.read
+          end
+
+          expect(data[:stderr]).to be_empty
+
+          expected = File.open(HOW_IS_EXAMPLE_REPOSITORY_JSON_REPORT).read.chomp
+          actual   = File.open('report.json').read.chomp
+
+          expect(expected).to eq(actual)
         }
       }
     end
