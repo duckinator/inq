8a1e81475cdb40404576e74f74e4208c1056308e
Ellen Marie Dash <me@duckie.co>
2016-12-11 00:56:05 -0500

    *slow clap* I forgot to configure VCR in Rakefile.

--- Rakefile
+++ Rakefile
@@ -1,7 +1,8 @@
 require 'bundler/gem_tasks'
 require 'rspec/core/rake_task'
 require 'timecop'
-require 'vcr'
+#require 'vcr'
+require './spec/vcr_helper.rb'
 require 'how_is'
 
 RSpec::Core::RakeTask.new(:spec)
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,11 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'how_is'
 require 'timecop'
-require 'vcr'
+#require 'vcr'
 
-VCR.configure do |config|
-  config.cassette_library_dir = "fixtures/vcr_cassettes"
-  config.hook_into :webmock
-end
+#VCR.configure do |config|
+#  config.cassette_library_dir = "fixtures/vcr_cassettes"
+#  config.hook_into :webmock
+#end
+
+require File.expand_path('./vcr_helper.rb', __dir__)
