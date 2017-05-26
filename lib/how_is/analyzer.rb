6e74ea020be7a28dcbf2c84c1d7f95e5f988b4af
Ellen Marie Dash <me@duckie.co>
2017-05-26 06:08:33 -0400

    [how_is/analyzer] remove trailing comma in method call

--- lib/how_is/analyzer.rb
+++ lib/how_is/analyzer.rb
@@ -54,7 +54,7 @@ class HowIs
         newest_issue: issue_or_pull_to_hash(newest_for(issues)),
         newest_pull: issue_or_pull_to_hash(newest_for(pulls)),
 
-        pulse: data.pulse,
+        pulse: data.pulse
       )
     end
 
