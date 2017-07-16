513d0082b1616f35517f0f56deb1aa976858e251
Olle Jonsson <olle.jonsson@gmail.com>
2017-07-16 16:38:22 +0200

    Avoid too long block

--- lib/how_is/report/base_report.rb
+++ lib/how_is/report/base_report.rb
@@ -3,9 +3,11 @@
 require 'json'
 
 class HowIs
+  BaseReport = Struct.new(:analysis)
+
   ##
   # Subclasses of BaseReport represent complete reports.
-  BaseReport = Struct.new(:analysis) do
+  class BaseReport
     def generate_report_text!
       # title, text, header, horizontal_bar_graph, etc,
       # append to @r, which is returned at the end of the function.
