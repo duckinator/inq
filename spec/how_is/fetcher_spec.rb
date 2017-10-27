a8b1777a45855798fa4672b81a1a4075ebc262e9
Ellen Marie Dash <me@duckie.co>
2017-10-27 16:49:53 -0400

    fix fetcher spec.

--- spec/how_is/fetcher_spec.rb
+++ spec/how_is/fetcher_spec.rb
@@ -11,9 +11,9 @@ describe HowIs::Fetcher do
     it "returns a hash containing issues and pull requests" do
       VCR.use_cassette("how_is_fetcher_call") do
         actual = subject.call("how-is/example-repository", "2017-08-01")
-        actual_json = actual.to_hash.to_json
+        actual_json = actual.to_hash.to_json.chomp
 
-        expected_json = File.read(FETCHER_SPEC_EXAMPLE_OUTPUT_FILE)
+        expected_json = File.read(FETCHER_SPEC_EXAMPLE_OUTPUT_FILE).chomp
 
         expect(actual_json).to eq(expected_json)
       end
