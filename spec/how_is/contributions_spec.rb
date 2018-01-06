cc223ca656a35661526a547a13a42a16e8426afd
Ellen Marie Dash <me@duckie.co>
2018-01-06 15:16:04 -0500

    fix test failure.

--- spec/how_is/contributions_spec.rb
+++ spec/how_is/contributions_spec.rb
@@ -110,8 +110,6 @@ describe HowIs::Sources::Github::Contributions do
         # rubocop:disable Metrics/LineLength
         summary = 'From Aug 01, 2017 through Sep 01, 2017, how-is/example-repository gained <a href="https://github.com/how-is/example-repository/compare/master@%7B2017-08-01%7D...master@%7B2017-09-01%7D">2 new commits</a>, contributed by 2 authors. There were 2 additions and 1 deletion across 1 file.'
         # rubocop:enable Metrics/LineLength
-        p contributions.summary
-        p summary
         expect(contributions.summary).to eq summary
       end
     end
