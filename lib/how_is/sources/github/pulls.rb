0876c618f1f120c993a12ad249f595027678833d
Ellen Marie Dash <me@duckie.co>
2018-04-27 16:01:18 -0400

    Resolve RuboCop warning.

--- lib/how_is/sources/github/pulls.rb
+++ lib/how_is/sources/github/pulls.rb
@@ -5,25 +5,27 @@ require "how_is/sources/github"
 require "how_is/sources/github_helpers"
 require "date"
 
-module HowIs::Sources
-  class Github
-    ##
-    # Fetches various information about GitHub Pull Requests
-    class Pulls < Issues
-      def url_suffix
-        "pulls"
-      end
+module HowIs
+  module Sources
+    class Github
+      ##
+      # Fetches various information about GitHub Pull Requests
+      class Pulls < Issues
+        def url_suffix
+          "pulls"
+        end
 
-      def singular_type
-        "pull"
-      end
+        def singular_type
+          "pull"
+        end
 
-      def type
-        "pullRequests"
-      end
+        def type
+          "pullRequests"
+        end
 
-      def pretty_type
-        "pull request"
+        def pretty_type
+          "pull request"
+        end
       end
     end
   end
