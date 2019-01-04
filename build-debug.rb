240b9f37d94554df228eed44b57fed8abe7a858b
Ellen Marie Dash <me@duckie.co>
2019-01-04 17:48:24 -0500

    update build-debug.rb script

--- build-debug.rb
+++ build-debug.rb
@@ -7,13 +7,13 @@ require "how_is/sources/ci/travis"
 require "how_is/sources/ci/appveyor"
 require "pp"
 
-travis = HowIs::Sources::CI::Travis.new("rubygems/rubygems", "2018-04-01", "2018-06-01")
+travis = HowIs::Sources::CI::Travis.new("rubygems/rubygems", "2018-12-01", "2019-02-01")
 builds = travis.builds
 puts "Number of builds: #{builds.length}"
 puts "First build:      #{builds.first['html_url']} (#{builds.first['started_at'].rfc3339})"
 puts "Last build:       #{builds.last['html_url']} (#{builds.last['started_at'].rfc3339})"
 
-appveyor = HowIs::Sources::CI::Appveyor.new("rubygems/rubygems", "2018-04-01", "2018-06-01")
+appveyor = HowIs::Sources::CI::Appveyor.new("rubygems/rubygems", "2018-12-01", "2019-02-01")
 builds = appveyor.builds
 puts "Number of builds: #{builds.length}"
 puts "First build:      #{builds.first['html_url']} (#{builds.first['started_at'].rfc3339})"
