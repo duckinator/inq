8f8fcce9bf6a304d5a9cb7200032dbd78047756a
Ellen Marie Dash <me@duckie.co>
2019-01-17 17:15:08 -0500

    Clean up ReportCollection.

--- lib/how_is/report_collection.rb
+++ lib/how_is/report_collection.rb
@@ -12,39 +12,29 @@ module HowIs
       @date = date
     end
 
-    def expand_filename(filename, report_data)
-      # Sometimes report_data has unused keys, which generates a warning, but
-      # we're okay with it, so we wrap it with silence_warnings {}.
-      silence_warnings { filename % report_data }
-    end
-    private :expand_filename
-
-    def prepare_report_metadata(repository, date)
-      end_date = DateTime.strptime(date, "%Y-%m-%d")
+    def metadata
+      end_date = DateTime.strptime(@date, "%Y-%m-%d")
       friendly_end_date = end_date.strftime("%B %d, %y")
 
       {
-        repository: repository,
+        repository: @repository,
         date: end_date,
         friendly_date: friendly_end_date,
       }
     end
-    private :prepare_report_metadata
-
-    # Export +report+ to the specified +format+,
-    # with the specified +frontmatter+.
-    def report_export(report, format, frontmatter)
-      report.send("to_#{format}", frontmatter)
-    end
-    private :report_export
+    private :metadata
 
     def to_h
-      metadata = prepare_report_metadata(@config["repository"], @date)
-
       @config["reports"].map { |format, report_config|
-        filename = expand_filename(report_config["filename"], metadata)
+        # Sometimes report_data has unused keys, which generates a warning, but
+        # we're okay with it, so we wrap it with silence_warnings {}.
+        filename = silence_warnings { report_config["filename"] % metadata }
+
         file = File.join(report_config["directory"], filename)
-        export = report_export(@report, format, report_config["frontmatter"])
+
+        # Export +report+ to the specified +format+ with the specified
+        # +frontmatter+.
+        export = @report.send("to_#{format}", report_config["frontmatter"])
 
         [file, export]
       }.to_h
