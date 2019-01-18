c43809de82936b114fc6c371e3ac2b22e0837da3
Ellen Marie Dash <me@duckie.co>
2019-01-17 20:34:46 -0500

    Clean up Sources::Github.new by adding a helper method.

--- lib/how_is/sources/github.rb
+++ lib/how_is/sources/github.rb
@@ -11,24 +11,20 @@ module HowIs
       class ConfigError < StandardError
       end
 
-      def initialize(global_config)
-        raise ConfigError, "Expected Hash, got #{global_config.class}." unless \
-          global_config.is_a?(Hash)
+      def initialize(config)
+        must_have_key!(config, "sources/github")
 
-        raise ConfigError, "No config for sources/github." unless \
-          global_config.has_key?("sources/github")
+        @config = config["sources/github"]
 
-        config = global_config["sources/github"]
-
-        raise ConfigError, "No username provided for sources/github." unless \
-          config.has_key?("username") && config["username"]
-
-        raise ConfigError, "No token provided for sources/github." unless \
-          config.has_key?("token") && config["token"]
-
-        @global_config = global_config
-        @config = config
+        must_have_key!(@config, "username")
+        must_have_key!(@config, "token")
       end
+
+      def must_have_key!(hash, key)
+        raise ConfigError, "Expected Hash, got #{hash.class}" unless hash.is_a?(Hash)
+        raise ConfigError, "Expected key `#{key}'" unless hash.has_key?(key)
+      end
+      private :must_have_key!
 
       # The GitHub username used for authenticating with GitHub.
       def username
