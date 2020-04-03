c0356a4c8b68c4a1027c503373cdff4b70d8234d
Ellen Marie Dash <me@duckie.co>
2020-04-03 18:47:39 -0400

    Have VCR filter authorization headers out of fixtures.

--- spec/vcr_helper.rb
+++ spec/vcr_helper.rb
@@ -7,13 +7,23 @@ VCR.configure do |config|
   vcr_mode = ENV["VCR_MODE"] =~ /rec/i ? :all : :once
 
   config.configure_rspec_metadata!
-  allow_http_connections_when_no_cassette = false
 
   config.default_cassette_options = {
     record: vcr_mode,
     match_requests_on: [:method, :uri, :body]
   }
 
+  # Filter out the values of 'Authorization:' headers.
+  config.filter_sensitive_data("<AUTHORIZATION TOKEN>") { |interaction|
+    auth_headers = interaction.request.headers["Authorization"]
+    if auth_headers.is_a?(Array) && auth_headers.length > 0
+      auth_headers.first
+    else
+      "<AUTHORIZATION TOKEN>" # idk if you can just return nil here without weirdness?
+    end
+  }
+
+  config.allow_http_connections_when_no_cassette = false
   config.cassette_library_dir = "fixtures/vcr_cassettes"
   config.hook_into :webmock
 end
