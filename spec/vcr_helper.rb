8b944d8f1405c1191490a66a58832364dae7b795
Ellen Marie Dash <me@duckie.co>
2020-04-03 17:16:13 -0400

    Configure VCR to ignore headers.

--- spec/vcr_helper.rb
+++ spec/vcr_helper.rb
@@ -3,8 +3,15 @@
 require "vcr"
 
 VCR.configure do |config|
+  # To have VCR re-record all casettes, do `VCR_MODE=rec bundle exec rake test`
+  vcr_mode = ENV["VCR_MODE"] =~ /rec/i ? :all : :once
+
+  config.configure_rspec_metadata!
+  allow_http_connections_when_no_cassette = false
+
   config.default_cassette_options = {
-    record: :new_episodes,
+    record: vcr_mode,
+    match_requests_on: [:method, :uri, :body]
   }
 
   config.cassette_library_dir = "fixtures/vcr_cassettes"
